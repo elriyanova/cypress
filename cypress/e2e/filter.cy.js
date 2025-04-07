35cda71358b08e90ce676100fe05593c3a7c7b2c
Ellie <admin@Ellies-MacBook-Pro.local>
2025-04-07 13:41:33 +0300

    Added tests for filter functionality

---
+++ cypress/e2e/filter.cy.js
@@ -0,0 +1,67 @@
+describe("Filter functionality", () => {
+  let userUrl;
+
+  beforeEach(function () {
+    cy.fixture("users").then((users) => {
+      this.users = users;
+      cy.login(this.users.filterUser.email, this.users.filterUser.password);
+
+      cy.generateUserUrl(this.users.filterUser.email).then((url) => {
+        userUrl = url;
+
+        cy.origin(userUrl, () => {
+          cy.url().should("not.include", "/auth/login");
+          cy.get('[data-testid="avatar-menu-btn"]').should("be.visible");
+        });
+      });
+    });
+  });
+
+  it("should show more than 1 deal when no filters are applied", () => {
+    cy.origin(userUrl, () => {
+      cy.get('[data-test="pipeline-deal-tile"]').its("length").should("be.gt", 1);
+    });
+  });
+
+  it("should apply a filter and show exactly 1 filtered deal", () => {
+    cy.origin(userUrl, () => {
+
+        // create filter
+        cy.get('[data-testid="filter-menu-button"]').click();
+        cy.get('[data-testid="add-filter-button"]').click();
+        cy.get('[data-testid="field-selector-option-label"]').click();
+        cy.get('[data-testid="labels-placeholder"]').click();
+        cy.get('[data-testid="labels-option-badge"]').click();
+        cy.get('[data-testid="save-button"]').click();
+
+        // verify filter show results
+        cy.get('[data-test="pipeline-deal-tile"]').should("be.visible").and("have.length", 1);
+
+        // delete filter
+        cy.get('[data-testid="edit-current-filter-button"]').click();
+        cy.get('[data-testid="delete-button"]').click();
+        cy.get('[data-testid="delete-confirmation-button"]').click();
+    });
+  });
+
+  it("should delete filter and show more than 1 deal again", () => {
+    cy.origin(userUrl, () => {
+        // create filter
+        cy.get('[data-testid="filter-menu-button"]').click();
+        cy.get('[data-testid="add-filter-button"]').click();
+        cy.get('[data-testid="field-selector-option-label"]').click();
+        cy.get('[data-testid="labels-placeholder"]').click();
+        cy.get('[data-testid="labels-option-badge"]').click();
+        cy.get('[data-testid="filter-name-input"]').type(filterName);
+        cy.get('[data-testid="save-button"]').click();
+
+        // delete filter
+        cy.get('[data-testid="edit-current-filter-button"]').click();
+        cy.get('[data-testid="delete-button"]').click();
+        cy.get('[data-testid="delete-confirmation-button"]').click();
+
+        // verify results updated after delete filter
+        cy.get('[data-test="pipeline-deal-tile"]').its("length").should("be.gt", 1);
+    });
+  });
+});
